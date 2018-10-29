56569122124bfa34d2de52cbadc5d89350106f3e
Melina Deraldo <melina@thoughtworks.com>
2018-10-29 09:44:09 -0300

    [Melina] Add third-party dependency at the project

--- notes-node/apps.js
+++ notes-node/apps.js
@@ -3,6 +3,7 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 var user = os.userInfo();
