71c953272bda7655852c9163c755938122ffdd13
melderaldo <melina.deraldo@gmail.com>
2018-11-04 17:03:44 -0200

    [Melina] Remove code to test how to use the os info

--- notes-node/apps.js
+++ notes-node/apps.js
@@ -2,19 +2,7 @@ console.log('Starting app.js');
 
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
+
 const notes = require('./notes.js');
-
-var user = os.userInfo();
-
-fs.appendFile('greeting.txt' , `Hello ${user.username}! You are ${notes.age}`, function(err) {
-
-    if(err){
-        console.log("err");
-    }
-});
-
-var result = notes.addNotes();
-
-console.log(result);
