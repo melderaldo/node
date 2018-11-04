52184df680cd8c7fc5b1114ac70856bc82371968
melderaldo <melina.deraldo@gmail.com>
2018-11-04 17:15:27 -0200

    [Mel] Add actions for notes

--- notes-node/apps.js
+++ notes-node/apps.js
@@ -6,3 +6,17 @@ const _ = require('lodash');
 
 
 const notes = require('./notes.js');
+
+var command = process.argv[2]
+
+if (command == 'add') {
+    console.log('Add new note');
+} else if (command = 'list ') {
+    console.log('List notes')
+} else if (command = 'delete') {
+    console.log('Delete note')
+} else if (command = 'read') {
+    console.log('Read note');
+} else {
+    console.log('Command not reconized');
+}
