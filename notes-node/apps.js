9be79d8ab9553ae9cb8a5887424ddf388823e6f8
melderaldo <melina.deraldo@gmail.com>
2018-11-04 17:25:36 -0200

    [Mel] Add yarg to get arguments from command line

--- notes-node/apps.js
+++ notes-node/apps.js
@@ -3,19 +3,20 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 
 const notes = require('./notes.js');
 
-var command = process.argv[2]
+var command = yargs.argv._[0];
 
 if (command == 'add') {
     console.log('Add new note');
-} else if (command = 'list ') {
+} else if (command == 'list ') {
     console.log('List notes')
-} else if (command = 'delete') {
+} else if (command == 'delete') {
     console.log('Delete note')
-} else if (command = 'read') {
+} else if (command == 'read') {
     console.log('Read note');
 } else {
     console.log('Command not reconized');
